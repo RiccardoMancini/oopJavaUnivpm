a8fe7d6d0981e472d66e03a75d205aa00b398126
Riccardo Mancini <riccardomancini98@gmail.com>
2019-09-24 22:23:00 +0200

    Aggiunto HelloWorld

---
+++ EsameOop/src/HelloWorld.java
@@ -0,0 +1,11 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String nome="Riccardo"; 
+		System.out.println("Hello, i'm "+nome+"!");
+	}
+
+}
