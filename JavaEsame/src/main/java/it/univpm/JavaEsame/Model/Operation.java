d32fd25e44da22e394d2cfa0997077e2e3d2953c
Riccardo Mancini <riccardomancini98@gmail.com>
2019-10-17 11:17:07 +0200

    UML e Comments

--- JavaEsame/src/main/java/it/univpm/JavaEsame/Model/Operation.java
+++ JavaEsame/src/main/java/it/univpm/JavaEsame/Model/Operation.java
@@ -19,54 +19,6 @@ public class Operation {
 		this.count = count;
 	}
 
-	public double getDev_std() {
-		return dev_std;
-	}
-
-	public void setDev_std(double dev_std) {
-		this.dev_std = dev_std;
-	}
-
-	public double getSum() {
-		return sum;
-	}
-
-	public void setSum(double sum) {
-		this.sum = sum;
-	}
-
-	public int getCount() {
-		return count;
-	}
-
-	public void setCount(int count) {
-		this.count = count;
-	}
-
-	public double getAvg() {
-		return avg;
-	}
-
-	public void setAvg(double avg) {
-		this.avg = avg;
-	}
-
-	public double getMax() {
-		return max;
-	}
-
-	public void setMax(double max) {
-		this.max = max;
-	}
-
-	public double getMin() {
-		return min;
-	}
-
-	public void setMin(double min) {
-		this.min = min;
-	}
-
 	
 
 	
